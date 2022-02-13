59569a5f8ab350c82ac4b45216571c780a444fc1
AhmedStohy <57235472+AhmedStohy@users.noreply.github.com>
2022-02-13 04:54:38 +0200

    Update local_variables.py

--- configs/local_variables.py
+++ configs/local_variables.py
@@ -1,8 +1,8 @@
 MOUNT_PATH = ""  # in case you are mounting data storage externally
 SPLIT = 'training'
 
-KITTI_WORK_DIR = MOUNT_PATH + "/storage/slurm/kimal/eagermot_workspace/kitti"
-KITTI_DATA_DIR = MOUNT_PATH + "/storage/slurm/osep/datasets/kitti"
+KITTI_WORK_DIR = "./"
+KITTI_DATA_DIR = "../input/kitti-3d-object-detection-dataset"
 
 NUSCENES_WORK_DIR = MOUNT_PATH + "/storage/slurm/kimal/eagermot_workspace/nuscenes"
 NUSCENES_DATA_DIR = MOUNT_PATH + "/storage/slurm/kimal/datasets_original/nuscenes"
